70f07ee2f10ccbaf91faae9904935fbc7dadd4dd
Mates Dušek <mat.dusek@gmail.com>
2025-03-10 00:11:58 +0100

    Re-worked scripts.js for changing between dark/light theme, still there is needed to change ids and classes and add some more for changing everything between themes, made card responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.getElementById("themeToggle");
     const body = document.body;
-    const textElement = document.getElementById("themeText");
+    const textElement = document.querySelectorAll(".themeText");
+    const activeUnderline = document.getElementById("themeActiveUnderline");
+    const H5Underline = document.getElementById("h5-underline-theme");
 
+        toggleButton.addEventListener("click", function () {
+            if (body.getAttribute("data-bs-theme") === "dark") {
+                textElement.forEach(element => {
+                    element.classList.remove("text-danger-subtle-dark");
+                    element.classList.add("text-danger-subtle-light");
+                });
+                
+                activeUnderline.classList.remove("active-dark");
+                activeUnderline.classList.add("active-light");
 
-    toggleButton.addEventListener("click", function () {
-        if (body.getAttribute("data-bs-theme") === "dark") {
-            textElement.classList.remove("text-danger-subtle-dark")
-            textElement.classList.add("text-danger-subtle-light");
-            body.setAttribute("data-bs-theme", "light");
+                H5Underline.classList.remove("h5-underline-danger-subtle-dark");
+                H5Underline.classList.add("h5-underline-danger-subtle-light");
 
-        } else {
-            textElement.classList.remove("text-danger-subtle-light");
-            textElement.classList.add("text-danger-subtle-dark");
-            body.setAttribute("data-bs-theme", "dark");
-        }
+                body.setAttribute("data-bs-theme", "light");
+            } else {
+                textElement.forEach(element => {
+                    element.classList.remove("text-danger-subtle-light");
+                    element.classList.add("text-danger-subtle-dark");
+                });
+
+                activeUnderline.classList.remove("active-light");
+                activeUnderline.classList.add("active-dark");
+
+                H5Underline.classList.remove("h5-underline-danger-subtle-light");
+                H5Underline.classList.add("h5-underline-danger-subtle-dark");
+
+                body.setAttribute("data-bs-theme", "dark");
+            }
+        });
     });
-});
 
 
 // toggleButton.addEventListener("click", function (){
