1b4f87c6aeb62d2e1a5707e56670138e3dc16e45
Mates Dušek <mat.dusek@gmail.com>
2025-03-10 20:45:40 +0100

    Dark/Light mode - Card Border, animated underline, active underline

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
     
     // Theme items for change
     const textElement = document.querySelectorAll(".themeText");
+    const cardBorder = document.querySelectorAll(".customCardBorder")
+    const animatedUnderline = document.querySelectorAll(".onHoverUnderline");
+
     const activeUnderline = document.getElementById("themeActiveUnderline");
-    const H5Underline = document.getElementById("h5-underline-theme");
+    const H5Underline = document.getElementById("customH5Underline");
 
     // Class switcher
     function switchClass(items, oldClass, newClass) {
@@ -19,12 +22,16 @@ document.addEventListener("DOMContentLoaded", () => {
     function switchTheme() {
         if (body.getAttribute("data-bs-theme") === "dark") {
             switchClass(textElement, "text-dark", "text-light");
+            switchClass(cardBorder, "card-border-dark", "card-border-light");
+            switchClass(animatedUnderline, "on-hover-dark", "on-hover-light");
             switchClass([activeUnderline], "active-dark", "active-light");
             switchClass([H5Underline], "h5-underline-dark", "h5-underline-light");
             // Change theme to light
             body.setAttribute("data-bs-theme", "light")
         } else {
             switchClass(textElement, "text-light", "text-dark");
+            switchClass(cardBorder, "card-border-light", "card-border-dark");
+            switchClass(animatedUnderline, "on-hover-light", "on-hover-dark");
             switchClass([activeUnderline], "active-light", "active-dark");
             switchClass([H5Underline], "h5-underline-light", "h5-underline-dark");
             // Change theme to dark
