48941bc042d1622739a82fec75b65434ac406ccd
Mates Dušek <mat.dusek@gmail.com>
2025-03-10 21:08:23 +0100

    Light mode for navbar background

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const cardBorder = document.querySelectorAll(".customCardBorder")
     const animatedUnderline = document.querySelectorAll(".onHoverUnderline");
 
+    const navBarBackground = document.getElementById("customNavBarBackground")
     const activeUnderline = document.getElementById("themeActiveUnderline");
     const H5Underline = document.getElementById("customH5Underline");
 
@@ -24,6 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
             switchClass(textElement, "text-dark", "text-light");
             switchClass(cardBorder, "card-border-dark", "card-border-light");
             switchClass(animatedUnderline, "on-hover-dark", "on-hover-light");
+            switchClass([navBarBackground], "bg-dark-subtle", "nav-bg-light");
             switchClass([activeUnderline], "active-dark", "active-light");
             switchClass([H5Underline], "h5-underline-dark", "h5-underline-light");
             // Change theme to light
@@ -32,6 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
             switchClass(textElement, "text-light", "text-dark");
             switchClass(cardBorder, "card-border-light", "card-border-dark");
             switchClass(animatedUnderline, "on-hover-light", "on-hover-dark");
+            switchClass([navBarBackground], "nav-bg-light", "bg-dark-subtle");
             switchClass([activeUnderline], "active-light", "active-dark");
             switchClass([H5Underline], "h5-underline-light", "h5-underline-dark");
             // Change theme to dark
